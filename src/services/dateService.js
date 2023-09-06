aea5fc8e5d6d89a4b12bf2d31667dad2aa26cc49
Enoque Rogerio <enoquerogerio15mti25@gmail.com>
2023-09-06 13:31:05 +0100

    method getCurrentDate implemented

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -1,7 +1,15 @@
 const getCurrentDate = () => {
-    // Add your solution here!
+  // Add your solution here!
+  const date = new Date();
+  const [year, month, day] = [
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+  ];
+  // Log to console
+  return `${year}-${month + 1}-${day}`;
 };
 
 module.exports = {
-    getCurrentDate
-}
+  getCurrentDate,
+};
